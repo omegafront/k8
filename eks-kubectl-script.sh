b833305d908809b0f8c35c1834ae787c7f136b8c
tom <tommchow@gmail.com>
2021-10-26 18:18:31 +0000

    added-eks-script

---
+++ eks-kubectl-script.sh
@@ -0,0 +1,9 @@
+#script to setup eksctl and kubectl
+#!/bin/bash
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
+echo 'eksctl and kubectl installed'
